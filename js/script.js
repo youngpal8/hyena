1493fa5c8e5f9ac223604aa0f70dcec9c59ef4fb
YOUNG-PAL <kat01brown@gmail.com>
2024-05-24 13:26:48 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// document.getElementById('amount').innerHtml = 'welocme to my first coding class'
+
+// to change the font size of an element
+document.getElementById('amount').style.fontSize= '80px';
+
+// to change the color of an element
+document.getElementById('amount').style.color= 'red';
+
+document.getElementById('fruit').style.display= 'none';
+
+function bulboff(){
+    document.getElementById('image').src='../images/bulb off.jpg'
+}
+
+function bulbon(){
+    document.getElementById('image').src='../images/bulb on.jpg'
+}
